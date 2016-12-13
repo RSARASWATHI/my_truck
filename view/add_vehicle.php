83f2f10e17f7fa54ffd45fee6cfcbed7cc585ac5
SARASWATHI <saraswathiec24@gmail.com>
2016-12-13 14:22:03 +0530

    add vehicle page design completed

---
+++ view/add_vehicle.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>otp verification</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> LOGIN PAGE</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <div class="logo">
+                    <img src="../images/image1.jpg" alt="mytruck">
+                    <span>my truck</span>
+                </div>
+            </div>
+        </div>
+    </nav>
+ <div id="wrapper">
+    
+        <div class="navbar navbar-inverse navbar-fixed-left" id="sidebar-wrapper" role="navigation">
+            <ul class="nav sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="add_vehicle_page.php">
+                       Add Vehicle
+                    </a>
+
+                </li>
+            </ul>
+        </div>
+        
+        
+        <div class="container">
+            <h1>welcome vefetch</h1>
+        </div>
+        </div>
+    
+        
+</body>
+
+</html>
