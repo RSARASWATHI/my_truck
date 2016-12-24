ad948b478b3bc6ac6752340940e73e343d80c586
DESKTOP-OMQTFCT\vefetch <saranyadharani121@gmail.com>
2016-12-24 12:32:54 +0530

    committed some changes

--- controllers/add_vehicle_controllers.php
+++ controllers/add_vehicle_controllers.php
@@ -4,11 +4,11 @@
 	include_once 'common_functions.php';
 	$data=$_POST;
 	add_vehicle_detail($data);
-	due_message_date($data);
+	// due_message_date($data);
 	function add_vehicle_detail($_get){
 		$con=db_connect();
 		insert('vehicle_detail',array('vehicle_no','RC_date' ,'Insurance_date', 'Due_date', 'Due_amount', 'Reminder_before', 'Due_interval', 'Total_dues','user_id'),array($_get['vehicle_no'],$_get['vehicle_RC_date'],$_get['insurance_date'], $_get['loan_due_date'],$_get['loan_due_amount'],$_get['reminder_before'],$_get['due_time_interval'],$_get['total_due'],$_get['id']),$con);
-		// header('Location:../view/add_vehicle.php');
+		header('Location:../view/add_vehicle.php');
 	}
 			
 	function due_message_date($_get){
