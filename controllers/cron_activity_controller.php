fad2229d481c3df09fba833d73b7c4c83d32a75c
Sudhakar Annadurai <asudhakar@live.in>
2017-01-05 15:47:07 +0530

    code reviewed reduced repeated codes

--- controllers/cron_activity_controller.php
+++ controllers/cron_activity_controller.php
@@ -6,9 +6,9 @@ include_once'../model/curd_operations.php';
 	function check_the_date($user_data, $keys){
 		foreach ($user_data as $key => $value) {
 			foreach ($keys as $key => $important_date_key) {
-				$Date=date("Y-m-d");
-				if($value[$important_date_key] ==  get_date($Date,6)){
-					process_this($value['id'], $important_date_key, $value['vehicle_no'], $value['Due_amount'], $value['Reminder_before'], $value['Due_interval'], $value['Total_dues'], $value['user_id']);
+				$date = date("Y-m-d");
+				if($value[$important_date_key] ==  get_date($date,$value['Reminder_before'])){
+					process_this($value['id'], $important_date_key, $value['vehicle_no'], $value['Due_amount'], $value['Reminder_before'], $value['Due_interval'], $value['Total_dues'], $value['user_id'], $value[$important_date_key]);
 				}else{
 					continue;
 				}
@@ -16,28 +16,25 @@ include_once'../model/curd_operations.php';
 		}
 	}
 
-	function process_this($vehicle_id, $db_slug, $vehicle_no, $due_ammount, $reminder_before, $due_interval, $total_dues, $user_id){
+	function process_this($vehicle_id, $db_slug, $vehicle_no, $due_ammount, $reminder_before, $due_interval, $total_dues, $user_id, $date){
 		switch($db_slug){
 			case 'RC_date':
-				// echo $db_slug;
-				change_date($db_slug,$vehicle_no);
+				echo $db_slug;
+				change_date($db_slug,$vehicle_no,$date);
 				break;
 			case 'Insurance_date':
-				// echo $db_slug;
-				change_date($db_slug,$vehicle_no);
+				echo $db_slug;
+				change_date($db_slug,$vehicle_no,$date);
 				break;
 			case 'Due_date':
 				echo $db_slug;
 				break;
 		}
 	}
-	function change_date($db_slug,$vehicle_no){
+	function change_date($db_slug,$vehicle_no,$date){
 		$db=db_connect();
 		$condition=" `vehicle_no` = '".$vehicle_no."'";
-		$date=select($db_slug,'vehicle_detail',$condition,$db);
-		// print_r($date[0][$db_slug]);
-		$Date = $date[0][$db_slug];
-		$change_dated=get_date($Date,365);
+		$change_dated=get_date($date,365);
 		// echo $change_dated;
 		update_date($change_dated,$db_slug,$vehicle_no,$db);
 	}
@@ -50,6 +47,6 @@ include_once'../model/curd_operations.php';
 			);
 			update($updated_details,'vehicle_detail',$condition,$db);
 		}
-	function get_date($Date,$value){
-		return date('Y-m-d', strtotime($Date.  '+'.$value .'days'));
+	function get_date($date,$value){
+		return date('Y-m-d', strtotime($date.  '+'.$value .'days'));
 	}
